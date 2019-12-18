457e1ecdf7ec3673dbb6cc32b355bb6fdea0a810
Jenna Miles <jennamiles@Jennas-MacBook-Pro.local>
2019-12-17 20:12:00 -0700

    adding my own stuff

---
+++ assets/javascript/niceAPI.js
@@ -0,0 +1,15 @@
+
+let queryUrl = 'http://quotes.rest/qod.json?category=inspire';
+
+$.ajax ({
+    url: queryUrl,
+    method: 'GET'
+}).then(function(response){
+    console.log('JENNA LOOK HERE' + response);
+    let quote = response.contents.quotes[0].quote;
+    let author = response.contents.quotes[0].author;
+
+    $('#quoteArea').html(quote);
+    $('#quoteAuthor').html(author);
+});
+
